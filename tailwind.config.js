de64799d163e4c5c9fde731b62984127f7dc1e05
Adam Akkari <105567700+ad3mo@users.noreply.github.com>
2023-11-19 15:30:05 +0100

    custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-blue": "#212A4D",
+        "primary-green": "#14CF9E",
+        "primary-gray": "F3F3F3",
+      },
+    },
   },
   plugins: [],
 };
