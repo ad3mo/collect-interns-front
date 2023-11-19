4c45fd3fd3fac949e9790adeb7503f2e626fc10a
Adam Akkari <105567700+ad3mo@users.noreply.github.com>
2023-11-19 16:27:13 +0100

    custom btns style, basic pages & routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   return (
     <div className="App">
-      <h1>Bonjour</h1>
+      <Router>
+        <Routes>
+          <Route index path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
