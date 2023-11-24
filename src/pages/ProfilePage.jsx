102812ba21d417895ed1ec74b0c900700c022f40
Adam Akkari <105567700+ad3mo@users.noreply.github.com>
2023-11-24 16:21:19 +0100

    profile settings grid layout

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -4,7 +4,16 @@ import MainLayout from '../components/MainLayout'
 const ProfilePage = () => {
   return (
     <MainLayout>
-      <h1>Profile</h1>
+      <main className='bg-white mt-8 mx-auto w-[80%] lg:w-[70%] p-8 flex flex-col gap-6 h-screen'>
+      <div class="grid grid-rows-3 grid-flow-col gap-5 m-5">
+      <div class="row-span-3 bg-gray-200 h-[250px] ">ProfilePic</div>
+      <div class="col-span-2 bg-gray-300">Profile Items</div>
+      <div class="row-span-2 col-span-2 bg-gray-400">Item Desc Setting</div>
+      </div>
+      <div className='bg-gray-200 h-[250px] m-5'>
+        preview
+      </div>
+      </main>
     </MainLayout>
   )
 }
