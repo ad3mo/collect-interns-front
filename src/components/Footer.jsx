58f0f419b2f819751caa3b2152d060ca71f9d5b4
Adam Akkari <105567700+ad3mo@users.noreply.github.com>
2023-11-24 16:35:23 +0100

    footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="h-[200px] bg-primary-green border-t-2 border-white">
+        <div className='mx-auto w-full maw-w-screen-xl p-4 py-6 lg:py-8'>
+                footer on the way
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 
 const MainLayout = ({ children }) => {
@@ -9,6 +10,7 @@ const MainLayout = ({ children }) => {
       <Navbar />
       
       {children}
+      <Footer />
     </div>
   );
 };
