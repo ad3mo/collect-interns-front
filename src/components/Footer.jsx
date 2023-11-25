f48b318b837fb62ca0c1bfea952e643c09e6f530
Adam Akkari <105567700+ad3mo@users.noreply.github.com>
2023-11-25 15:01:01 +0100

    footer & profile page incomplete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { FaInstagram, FaFacebook, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="h-[200px] bg-primary-green border-t-2 border-white">
-        <div className='mx-auto w-full maw-w-screen-xl p-4 py-6 lg:py-8'>
-                footer on the way
+    <footer className="h-[250px] bg-primary-green ">
+      <div className='mx-auto max-w-screen-xl p-4 py-6 lg:py-8 flex flex-col md:flex-row items-center justify-center md:justify-between'>
+        
+        
+        <div className='text-white text-center md:text-left mb-4 md:mb-0'>
+          <h1 className='text-2xl font-bold uppercase text-primary-blue'>Stage Embauche</h1>
+          <p>&copy; 2023 Collect Interns</p>
         </div>
-    </footer>
-  )
-}
 
-export default Footer
+        
+        <div className='flex flex-col md:flex-row items-center'>
+          
+          <div className='md:mr-8 mb-4 md:mb-0 text-white'>
+            <p className='mb-2'>Stage Embauche</p>
+            <p className='mb-2'>Monastir 5000</p>
+            <p>Monastir, Tunisia</p>
+          </div>
+
+         
+          <div className='flex items-center space-x-4 text-2xl bg-primary-green '>
+            <a href='#' target='_blank' rel='noopener noreferrer'>
+              <FaInstagram className='text-white hover:text-primary-blue' />
+            </a>
+            <a href='#' target='_blank' rel='noopener noreferrer'>
+              <FaFacebook className='text-white hover:text-primary-blue' />
+            </a>
+            <a href='#' target='_blank' rel='noopener noreferrer'>
+              <FaLinkedin className='text-white hover:text-primary-blue' />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,21 +1,82 @@
-import React from 'react'
-import MainLayout from '../components/MainLayout'
+import React, { useState } from 'react';
+import MainLayout from '../components/MainLayout';
+import kais from '../assets/kaisoun.jpg';
+import { FiCamera } from 'react-icons/fi';
 
 const ProfilePage = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [selectedElement, setSelectedElement] = useState('');
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleElementClick = (element) => {
+    setSelectedElement(element);
+    setInputValue('');
+  };
+
+  const handlePicHover = () => {
+    setIsHovered(!isHovered);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+   
+    console.log('Submitted:', inputValue, selectedElement);
+
+   
+  };
+
   return (
     <MainLayout>
-      <main className='bg-white mt-8 mx-auto w-[80%] lg:w-[70%] p-8 flex flex-col gap-6 h-screen'>
-      <div class="grid grid-rows-3 grid-flow-col gap-5 m-5">
-      <div class="row-span-3 bg-gray-200 h-[250px] ">ProfilePic</div>
-      <div class="col-span-2 bg-gray-300">Profile Items</div>
-      <div class="row-span-2 col-span-2 bg-gray-400">Item Desc Setting</div>
-      </div>
-      <div className='bg-gray-200 h-[250px] m-5'>
-        preview
-      </div>
+      <main className='bg-white mt-8 mx-auto w-full md:w-[80%] lg:w-[70%] p-4 md:p-8 flex flex-col gap-4 min-h-screen'>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-8'>
+          <div className='bg-gray-200 h-[250px] w-full md:w-[250px] flex overflow-hidden justify-center relative'>
+            <img
+              src={kais}
+              alt='kaispic'
+              className={`max-h-[250px] max-w-[250px] rounded-full object-cover grow transition-all ${isHovered ? 'filter blur-md flex justify-center ite' : ''}`}
+              onMouseEnter={handlePicHover}
+              onMouseLeave={handlePicHover}
+            />
+            {isHovered && (
+              <label
+                htmlFor='file-input'
+                className='absolute bottom-2 right-2 cursor-pointer bg-white p-2 rounded-full'
+              >
+                <FiCamera />
+                <input type='file' id='file-input' className='hidden' />
+              </label>
+            )}
+          </div>
+          <div className='col-span-2 bg-gray-300 flex items-center justify-center p-4 md:w-full'>
+            <ul className='flex flex-col md:flex-row items-center justify-between gap-3'>
+              <li className='cursor-pointer' onClick={() => handleElementClick('Nom')}>Nom</li>
+              <li className='cursor-pointer' onClick={() => handleElementClick('Institut')}>Institut</li>
+              <li className='cursor-pointer' onClick={() => handleElementClick('Email')}>Email</li>
+              <li className='cursor-pointer' onClick={() => handleElementClick('Téléphone')}>Téléphone</li>
+              <li className='cursor-pointer' onClick={() => handleElementClick('Spécialité')}>Spécialité</li>
+              <li className='cursor-pointer' onClick={() => handleElementClick('CV')}>CV</li>
+            </ul>
+          </div>
+        </div>
+        <div className='bg-gray-400 p-4 md:col-span-2 w-full md:w-[70%]'>
+          <form onSubmit={handleSubmit}>
+            <input
+              type='text'
+              placeholder={`Changer ${selectedElement}`}
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              className='w-full input-blue'
+            />
+            <button type='submit' className='m-2 primary-blue-btn'>Submit</button>
+          </form>
+        </div>
+        <div className='bg-gray-200 h-[250px] m-4 md:m-5'>
+          preview
+        </div>
       </main>
     </MainLayout>
-  )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
